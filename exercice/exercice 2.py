3373464663e8b839a160251add62f7d3b4337d86
fvsunny <fee1501@hotmail.com>
2026-01-30 17:50:19 -0500

    projet

---
+++ exercice/exercice 2.py
@@ -0,0 +1,6 @@
+x = 50
+y = 100
+temporaire = y
+y = x
+x = temporaire
+print(x, y)
---
+++ exercice/semaine 1.py
@@ -0,0 +1,5 @@
+nombre1 = 10
+nombre2 = nombre1 * nombre1
+nombre1 = 40
+nombre3 = nombre2 - nombre1 - 10
+print(nombre1, nombre2, nombre3)
