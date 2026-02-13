9eb1bcdb3ceefcde1517f510f180a82f0033ebd0
fvsunny <fee1501@hotmail.com>
2026-02-13 16:28:10 -0500

    cours 3

---
+++ exercice/semaine 3/énergie cinétique/science_nat.py
@@ -0,0 +1,4 @@
+def energie_cinetique(m,v):
+    E= (m*v^2)/2
+    return E
+
---
+++ exercice/semaine 3/énergie cinétique/script.py
@@ -0,0 +1,9 @@
+import science_nat
+
+print(science_nat.energie_cinetique(5,2))
+print(science_nat.energie_cinetique(8,4))
+print(science_nat.energie_cinetique(26,1))
+
+
+
+
